4596599d38719e997b3401ded8630a0160e868cc
BoillotFrederic <boillot.frederic.62@gmail.com>
2017-04-20 15:47:14 +0200

    Products

--- src/AppBundle/Controller/TestController.php
+++ src/AppBundle/Controller/TestController.php
@@ -10,12 +10,19 @@ use Symfony\Component\HttpFoundation\Response;
 class TestController extends Controller{
 
   /**
-  * @Route("test", name="test")
+  * @Route("/test/{username}", name="test", requirements={"username": "\D+"})
   */
-  public function indexAction(Request $request){
+  public function indexAction(Request $request, $username = 'Test'){
     return $this->render('test/test.html.twig', [
-      'userName' => 'Frédéric'
+      'userName' => $username
     ]);
+  }
+
+  /**
+  * @Route("/test/{username}/{page}", name="test_show", requirements={"page": "\d+"})
+  */
+  public function showAction($page){
+    return new Response('Ma page est ' . $page);
   }
 
 }
